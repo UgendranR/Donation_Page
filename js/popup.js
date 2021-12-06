980b1c844ac4f8d97207e07da943d484c07188c4
Ugendran R <suryacr7007@gmail.com>
2021-12-06 17:36:20 +0530

    page

---
+++ js/popup.js
@@ -0,0 +1,15 @@
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+  }
+  function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+     
+  }
+  window.onload = function() {
+    closeForm();
+  }
+
+
+
+
+
